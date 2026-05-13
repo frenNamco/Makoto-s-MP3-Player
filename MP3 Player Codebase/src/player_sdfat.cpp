c27aa72d047fa58d08f5e6d175f09f3abac52d32
Fernando Aragon <faragonchavarria3@gmail.com>
2026-05-13 15:29:21 -0400

    added button functionality and added play/pause feature

--- MP3 Player Codebase/src/player_sdfat.cpp
+++ MP3 Player Codebase/src/player_sdfat.cpp
@@ -14,6 +14,11 @@
 #define SDCS  21
 #define DREQ  26
 
+#define W_BUTTON_PIN  15
+#define R_BUTTON_PIN  14
+#define G_BUTTON_PIN  13
+#define B_BUTTON_PIN  12
+
 // SdFat object
 SdFat SD;
 
@@ -45,10 +50,23 @@ void printDirectory(FsFile dir, int numTabs) {
   }
 }
 
+void checkButtonPress(int &wBtnPress, int &rBtnPress, int &gBtnPress, int &bBtnPress) {
+  wBtnPress = !(digitalRead(W_BUTTON_PIN));
+  rBtnPress = !(digitalRead(R_BUTTON_PIN));
+  gBtnPress = !(digitalRead(G_BUTTON_PIN));
+  bBtnPress = !(digitalRead(B_BUTTON_PIN));
+}
+
 void setup() {
   Serial.begin(9600);
   while (!Serial) delay(1);
   delay(1000);
+
+  // Button initialization
+  pinMode(W_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(R_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(G_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(B_BUTTON_PIN, INPUT_PULLUP);
 
   Serial.println("VS1053 + SdFat Test");
 
@@ -84,6 +102,14 @@ void setup() {
   mp3.setVolume(20, 20);
 }
 
+int wBtnPress = 0;
+int rBtnPress = 0;
+int gBtnPress = 0;
+int bBtnPress = 0;
+
+bool musicStarted = false;
+bool currentTrackPaused = false;
+
 void loop() {
   // Serial.println("Trying TRACK001.MP3");
 
@@ -92,18 +118,49 @@ void loop() {
   //   while (1);
   // }
 
+  
   Serial.println("Trying ミッドナイト・ランデブー.mp3");
-
-  if (!mp3.startPlayingFile("/ミッドナイト・ランデブー.mp3")) {
-    Serial.println("Could not open ミッドナイト・ランデブー.mp3");
-    while (1);
+  
+  if (!musicStarted) {
+    if (!mp3.startPlayingFile("/ミッドナイト・ランデブー.mp3")) {
+      Serial.println("Could not open ミッドナイト・ランデブー.mp3");
+      while (1);
+    } else {
+      musicStarted = true;
+      currentTrackPaused = false;
+    }
   }
-
+  
   Serial.println("Started playing");
+  
+  while (mp3.playingMusic || musicStarted) {
+    checkButtonPress(wBtnPress, rBtnPress, gBtnPress, bBtnPress);
 
-  while (mp3.playingMusic) {
-    Serial.print(".");
-    delay(1000);
+    // Pause button func
+    if (wBtnPress) {
+      Serial.println("White button press");
+
+      mp3.pausePlaying(!currentTrackPaused);
+
+      currentTrackPaused = !currentTrackPaused;
+
+      delay(500);
+    }
+
+    if (rBtnPress) {
+      Serial.println("Red button press");
+      delay(500);
+    }
+
+    if (gBtnPress) {
+      Serial.println("Green button press");
+      delay(500);
+    }
+
+    if (bBtnPress) {
+      Serial.println("Blue button press");
+      delay(500);
+    }
   }
 
   Serial.println("\nDone playing music");
